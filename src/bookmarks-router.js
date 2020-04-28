993ddfaf4e5bb1a18173f863748271899cd0a90f
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2020-04-28 11:20:55 -0700

    created logger file

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('./logger');
 const bookmarksRouter = express.Router();
 const bodyParser = express.json();
 const { v4: uuid } = require('uuid');
@@ -12,18 +13,23 @@ bookmarksRouter
         res
             .status(200)
             .json(bookmarks)
-    })
-    .post(bodyParser, (req, res) => {
-        const { title, url, rating, description } = req.body;
+    }) 
+    .post(bodyParser, (req, res) => { //route handler for POST /bookmarks accepts a JSON object and adds it to bookmarks-data 
+        const { title, url, rating, description } = req.body; //get POST data with the .body method; use JSON middleware to parse body of request
+    //validate requests have the required parameters
         if(!title) {
+            logger.error(`A title is required.`);
             return res.status(400).send('Please provide a title.');
         }
         if(!url) {
+            logger.error(`url is required.`);
             return res.status(400).send('Please provide a url.');
         }
         if(!rating || isNaN(rating)) {
+            logger.error(`A valid number is required`);
             return res.status(400).send('Please provide a number between 1 and 5.');
         }
+    //if the request is valid, construct a bookmark with the request data and UUID generated ID
         const id = uuid()
         const newBookmark =  {
             id,
@@ -32,8 +38,9 @@ bookmarksRouter
             rating,
             description
         }
-        bookmarks["bookmarks"].push(newBookmark);
-        res.status(202).location(`http://localhost:8000/bookmarks/${id}`).json(newBookmark);
+        bookmarks["bookmarks"].push(newBookmark);//push the new bookmark object into the bookmarks data file
+        logger.info(`Bookmarks ID ${id} created!`);
+        res.status(202).location(`http://localhost:8000/bookmarks/${id}`).json(newBookmark); //return a status of created, with new bookmark info
         
     });
 
